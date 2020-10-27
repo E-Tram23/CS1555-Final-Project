3be927658fa3b4da028b576544ae16e7a0b5c09d
Eric Tram <ewt6@pitt.edu>
2020-10-27 15:58:47 -0400

    Update team20-hw5-query.sql

--- team20-hw5-query.sql
+++ team20-hw5-query.sql
@@ -58,3 +58,43 @@ $$ LANGUAGE plpgsql;
 
 --TESTING
 SELECT isPlaneFull(2);
+
+
+
+
+--Task 4 makeReservation
+
+CREATE OR REPLACE PROCEDURE makeReservation(res_num integer, flight_num integer, dep_date date, l integer)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    f_time varchar;
+    hour integer;
+    minute integer;
+    final_time time;
+    f_timedate timestamp;
+BEGIN
+    SELECT departure_time INTO f_time
+    FROM FLIGHT
+    WHERE flight_number = $2;
+
+    SELECT SUBSTRING(f_time from 1 for 2) INTO hour;
+
+    SELECT SUBSTRING(f_time from 3 for 2) INTO minute;
+
+    SELECT make_time(hour,minute,0.0) INTO final_time;
+
+    SELECT ($3 + final_time) INTO f_timedate;
+
+    INSERT INTO RESERVATION_DETAIL(reservation_number, flight_number, flight_date, leg) VALUES ($1,$2,f_timedate,$4);
+END;
+$$;
+
+--Test Inserts the reservation number 6 then updates and adds tuple into reservation_details table
+
+INSERT INTO RESERVATION(reservation_number, cid, cost, credit_card_num, reservation_date, ticketed) VALUES(7,5,615,'2538244543760285','10-28-2020 15:50:15.000000',FALSE);
+
+BEGIN;
+SET CONSTRAINTS ALL DEFERRED;
+CALL makeReservation(7, 3, TO_DATE('11-23-2020','MM-DD-YYYY'),1);
+COMMIT;
